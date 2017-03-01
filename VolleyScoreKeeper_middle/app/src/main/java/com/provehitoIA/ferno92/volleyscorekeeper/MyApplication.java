488aacf41ac99f6670d3b545119c3145ed98e83b
Luca Fernandez <luca.fernandez@spot.it>
2017-03-01 14:00:28 +0100

    add stetho

---
+++ VolleyScoreKeeper_middle/app/src/main/java/com/provehitoIA/ferno92/volleyscorekeeper/MyApplication.java
@@ -0,0 +1,37 @@
+package com.provehitoIA.ferno92.volleyscorekeeper;
+
+import android.app.Application;
+import android.content.res.Configuration;
+
+import com.facebook.stetho.Stetho;
+
+/**
+ * Created by luca.fernandez on 01/03/2017.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+                        .build());
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
